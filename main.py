8090d50eff267450635d408706139068d83d289d
Muhammad jasim <j1s9mj@gmail.com>
2024-07-11 13:03:33 +0530

    Day 4

--- main.py
+++ main.py
@@ -3,3 +3,13 @@ from bank_operations.bank import credit,debit,transfer
 
 print("Hello world")
 print("New program")
+
+
+class Fruits:
+    def __init__(self,name,color) ->None:
+        self.name=name
+        self.color=color
+
+    def say_color(self):
+        print("My color is ",self.color)
+        
